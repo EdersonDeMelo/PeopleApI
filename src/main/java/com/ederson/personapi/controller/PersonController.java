2e7a98a07513286719f772cf7fbc5c73beecd7c6
EdersonDeMelo <drsnmelo@gmail.com>
2021-06-24 21:08:26 -0300

    Implementado metodos criacao, selecao e delecao de usuario

--- src/main/java/com/ederson/personapi/controller/PersonController.java
+++ src/main/java/com/ederson/personapi/controller/PersonController.java
@@ -1,28 +1,50 @@
 package com.ederson.personapi.controller;
 
 
+import com.ederson.personapi.dto.request.PersonDTO;
 import com.ederson.personapi.dto.response.MessageResponseDTO;
 import com.ederson.personapi.entity.Person;
+import com.ederson.personapi.exception.PersonNotFoundException;
 import com.ederson.personapi.repository.PersonRepository;
 import com.ederson.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/people")
 public class PersonController {
 
 
-  private PersonService personService;
+    private PersonService personService;
 
-  @Autowired
+    @Autowired
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person){
-      return personService.createPerson(person);
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
+      return personService.createPerson(personDTO);
+    }
+
+    @GetMapping
+    public List<PersonDTO>listAll(){
+    return personService.listAll();
+    }
+
+    @GetMapping("/{id}")
+  public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+      return personService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.delete(id);
     }
 }
--- src/main/java/com/ederson/personapi/service/PersonService.java
+++ src/main/java/com/ederson/personapi/service/PersonService.java
@@ -1,26 +1,56 @@
 package com.ederson.personapi.service;
 
+import com.ederson.personapi.dto.request.PersonDTO;
 import com.ederson.personapi.dto.response.MessageResponseDTO;
 import com.ederson.personapi.entity.Person;
+import com.ederson.personapi.exception.PersonNotFoundException;
+import com.ederson.personapi.mapper.PersonMapper;
 import com.ederson.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PersonService {
 
     private PersonRepository personRepository;
 
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
+
     @Autowired
     public PersonService(PersonRepository personRepository) {
         this.personRepository = personRepository;
     }
-    public MessageResponseDTO createPerson(Person person){
-        Person savePerson = personRepository.save(person);
+
+    public MessageResponseDTO createPerson(PersonDTO personDTO){
+        Person personToSave = personMapper.toModel(personDTO);
+
+        Person savePerson = personRepository.save(personToSave);
         return MessageResponseDTO
                 .builder()
                 .message("Criada pessoa com ID: "+ savePerson.getId())
                 .build();
     }
 
+    public List<PersonDTO> listAll() {
+        List<Person>allPeople = personRepository.findAll();
+        return allPeople.stream().map(personMapper::toDTO).collect(Collectors.toList());
+
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person person = personRepository.findById(id)
+                .orElseThrow(()->new PersonNotFoundException(id));
+
+        return personMapper.toDTO(person);
+    }
+
+    public void delete(Long id) throws PersonNotFoundException {
+        personRepository.findById(id)
+                .orElseThrow(()-> new PersonNotFoundException(id));
+        personRepository.deleteById(id);
+    }
 }
